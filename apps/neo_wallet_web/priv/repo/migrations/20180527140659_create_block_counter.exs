bc2b09a671b717a688c4b26f1b7b3a6aa739a1ab
LoranceChen <w68055010@qq.com>
2018-05-29 15:31:37 +0800

    refine migrate

--- apps/neo_wallet_web/priv/repo/migrations/20180527140659_create_block_counter.exs
+++ apps/neo_wallet_web/priv/repo/migrations/20180527140659_create_block_counter.exs
@@ -1,14 +1,23 @@
 defmodule NeoWalletWeb.Repo.Migrations.CreateBlockCounter do
   use Ecto.Migration
 
-  def change do
+  def up do
     create table(:block_counter, primary_key: false) do
       add :current_count, :integer
 
       timestamps()
     end
 
+    flush()
+
+    initCount = %NeoWalletWeb.Dao.BlockCounter{current_count: -1}
+    NeoWalletWeb.Repo.insert(initCount)
   
   end
+
+  def down do
+    drop table(:block_counter)
+  end
+  
 
 end
--- apps/neo_wallet_web/priv/repo/migrations/20180529061013_init_block_counter.exs
+++
@@ -1,8 +0,0 @@
-defmodule NeoWalletWeb.Repo.Migrations.InitBlockCounter do
-  use Ecto.Migration
-
-  def up do
-    initCount = %NeoWalletWeb.Dao.BlockCounter{current_count: -1}
-    NeoWalletWeb.Repo.insert(initCount)
-  end
-end
